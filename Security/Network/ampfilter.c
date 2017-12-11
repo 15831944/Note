102f267e86f7fc62d406b6da9f8ddc76da098b23
vforbox <vforbox@gmail.com>
2017-12-11 12:15:30 +0800

    放大反射列表过滤

--- Security/Network/ampfilter.c
+++ Security/Network/ampfilter.c
@@ -171,18 +171,18 @@ int main(int argc, char *argv[])
     if (argc < 4)
     {
         fprintf(stdout, "Coded by Spai3N\nUsage: %s [list.txt] [output.txt] [Threads] [MinRespBytes]\n", argv[0]);
-        exit(-1); 
+        exit(-1);
     }
-   	filename = argv[1];
-   	loadfile();
-   	fd = fopen(argv[2], "a");
-   	signal(SIGINT, &sighandler);
-   	int threads = atoi(argv[3]);
-   	atleastbytes = atoi(argv[4]);
-   	pthread_t thread;
-   	pthread_t listenthread;
-   	pthread_create(&listenthread, NULL, &recievethread, NULL);
-   	int i;
+    filename = argv[1];
+    loadfile();
+    fd = fopen(argv[2], "a");
+    signal(SIGINT, &sighandler);
+    int threads = atoi(argv[3]);
+    atleastbytes = atoi(argv[4]);
+    pthread_t thread;
+    pthread_t listenthread;
+    pthread_create(&listenthread, NULL, &recievethread, NULL);
+    int i;
     for (i = 0; i < threads; i++)
     {
         pthread_create(&thread, NULL, &flood, (void *)i);
